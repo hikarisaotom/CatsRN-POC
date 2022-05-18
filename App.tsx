cf7c4133c3580151c55a0c3ae3bd24194aa0bdf1
hikarisaotom <clau_cortes10@hotmail.com>
2022-05-17 22:53:38 -0600

    Slideshow

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
 import { TabsNavigator } from './src/navigators/TabsNavigator';
+import { PricipalNavigator } from './src/navigators/PricipalNavigator';
 
 
 const App = () => {
   return (
     <NavigationContainer >
-      <TabsNavigator />
+     {/* <PrincipalStackNavigator/> */}
+     {/* <TabsNavigator/> */}
+     {/* <SlidesScreen/> */}
+     <PricipalNavigator/>
       </NavigationContainer>
   )
 }
---
+++ src/navigators/PricipalNavigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack';
+import { SlidesScreen } from '../screens/introductioSlides';
+import { TabsNavigator } from './TabsNavigator';
+const Stack = createStackNavigator();
+
+export const PricipalNavigator = () => {
+  return (
+    <Stack.Navigator
+        screenOptions={{
+            headerShown: false,
+            cardStyle: {
+              backgroundColor: 'white'
+            }
+        }}
+    >
+     <Stack.Screen name="InitialHomeScreen" component={ SlidesScreen } />
+     <Stack.Screen name="TabsNavigatorHomeScreen" component={ TabsNavigator } />
+
+    </Stack.Navigator>
+  );
+}
